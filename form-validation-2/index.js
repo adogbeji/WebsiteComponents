7e2af75f7bdd73412cb4d88454b670f4c4261f3a
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-01-02 11:47:17 +0000

    Update JavaScript code for Form Validation 2

---
+++ form-validation-2/index.js
@@ -0,0 +1,96 @@
+// jshint esversion:6
+
+const form = document.getElementById('form');
+const yourName = document.getElementById('clientName');
+const email = document.getElementById('clientEmail');
+const password_1 = document.getElementById('clientPassword1');
+const password_2 = document.getElementById('clientPassword2');
+
+form.addeventListener('submit', function(e) {  // The 'function' keyword ensures Internet Explorer compatability
+  checkInputs(e);  // Function that checks form inputs
+});
+
+function checkInputs(e) {
+  // Get values from inputs
+  const nameValue = yourName.value.trim();
+  const emailValue = email.value.trim();
+  const firstPasswordValue = password_1.value.trim();
+  const secondPasswordValue = password_2.value.trim();
+
+
+  // Name
+  if (nameValue === '') {
+    e.preventDefault();  // Prevents Form Submission
+    // Show error message + add error class
+    setErrorFor(yourName, 'Name cannot be blank!');
+  } else {
+    // Add success class
+     setSuccessFor(yourName);
+  }
+
+  // Email
+  if (emailValue === '') {
+    e.preventDefault();  // Prevents Form Submission
+    // Show error message + add error class
+    setErrorFor(email, 'Email cannot be blank!');
+  } else if (!validateEmail(emailValue)) {
+    e.preventDefault();  // Prevents Form Submission
+    setErrorFor(email, 'Email is not valid!');
+  } else {
+    // Add success class
+    setSuccessFor(email);
+  }
+
+  // Password 1
+  if (firstPasswordValue === '') {
+    e.preventDefault();  // Prevents Form Submission
+    // Show error message + add error class
+    setErrorFor(password_1, 'Password cannot be blank!');
+  } else if (password_1.value.length < 6) {
+    e.preventDefault();  // Prevents Form Submission
+    setErrorFor(password_1, 'Password must be at least 6 characters!');
+  } else if (password_1.value.length >= 20) {
+    e.preventDefault();  // Prevents Form Submission
+    setErrorFor(password_1, 'Password must be less than 20 characters!');
+  } else {
+    // Add success class
+    setSuccessFor(password_1);
+  }
+
+  // Password 2
+  if (secondPasswordValue === '') {
+    e.preventDefault();  // Prevents Form Submission
+    // Show error message + add error class
+    setErrorFor(password_2, 'Please confirm password!');
+  } else if (firstPasswordValue !== secondPasswordValue) {
+    e.preventDefault();  // Prevents Form Submission
+    setErrorFor(password_2, 'Passwords do not match!');
+  } else {
+    // Add success class
+    setSuccessFor(password_2);
+  }
+}
+
+
+function setErrorFor(input, message) {
+  const formGroup = input.parentElement;  // Div with class '.form-group'
+  const smallTag = formGroup.querySelector('.error-message');  // Small tag with class '.error-message'
+
+  // Add error message inside small tag
+  smallTag.style.display = 'inline-block';  // Allows block-element behaviour but remains inline
+  smallTag.innerText = message;
+  setTimeout(removeMessage, 2500);  // Removes error message after 2.5s
+
+  // Add error class
+  formGroup.className = 'form-group error';
+  setTimeout(removeClass, 2500);  // Removes error class after 2.5s
+
+  function removeMessage() {
+    smallTag.style.display = 'none';  // Hides small tag & removes it from DOM
+    smallTag.innerText = '';  // Removes error message text
+  }
+
+  function removeClass() {
+    formGroup.className = 'form-group';
+  }
+}
