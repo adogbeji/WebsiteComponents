ae48eeea6254f653ee82b311f74a4ad7d9c9cf52
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-05-18 11:55:57 +0100

    Add Email and Password verification to Form Validation 3

--- form-validation-3/index.js
+++ form-validation-3/index.js
@@ -28,4 +28,35 @@ function checkInputs(e) {
     // Add success class
     setSuccessFor(yourName);
   }
+
+
+  // Email
+  if (emailValue === '') {
+    e.preventDefault();  // Prevents Form Submission
+    // Show error message + add error class
+    setErrorFor(email, 'Email cannot be blank!');
+  } else if (!validateEmail(emailValue)) {
+    e.preventDefault();  // Prevents Form Submission
+    setErrorFor(email, 'Email is not valid!');
+  } else {
+    // Add success class
+    setSuccessFor(email);
+  }
+
+
+  // Password 
+  if (passwordValue === '') {
+    e.preventDefault();  // Prevents Form Submission
+    // Show error message + add error class
+    setErrorFor(password, 'Password cannot be blank!');
+  } else if (password.value.length < 6) {
+    e.preventDefault();  // Prevents Form Submission
+    setErrorFor(password, 'Password must be at least 6 characters!');
+  } else if (password.value.length >= 20) {
+    e.preventDefault();  // Prevents Form Submission
+    setErrorFor(password, 'Password must be less than 20 characters!');
+  } else {
+    // Add success class
+    setSuccessFor(password);
+  }
 }
