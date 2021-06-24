8f9e1cea34d25cf40b883809224d7be9d902a525
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-06-24 11:03:31 +0100

    Start adding JavaScript for Form Validation 4

---
+++ form-validation-4/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+
+const form = document.getElementById('form');
+const email = document.getElementById('clientEmail');
+const password_1 = document.getElementById('clientPassword_1');
+const password_2 = document.getElementById('clientPassword_2');
