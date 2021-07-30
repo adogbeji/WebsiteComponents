411360dccd6efdafd9457910f5eef49e4f1343b2
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-07-30 16:29:49 +0100

    Add JavaScript to validate email in Form Validation 4

--- form-validation-4/index.js
+++ form-validation-4/index.js
@@ -99,3 +99,9 @@ function setSuccessFor(input) {
    formGroup.style.marginBottom = '16px';  // Restores bottom margin
  }
 }
+
+
+// Special Function to validate email - returns true or false
+function validateEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
